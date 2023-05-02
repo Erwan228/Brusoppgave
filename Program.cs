88766e6617bd2ff2133751c30bce2ac2b33c82fe
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-05-02 14:36:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Brusoppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
